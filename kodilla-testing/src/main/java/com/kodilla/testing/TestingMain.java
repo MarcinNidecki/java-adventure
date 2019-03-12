e25780aed28baafe0cbee4b226559450831fa65a
MarcinNidecki <marcin.nidecki90@gmail.com>
2019-03-12 19:16:49 +0100

    Firt test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,4 +1,35 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
+    public static void main(String[] args){
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        int addvalue = calculator.add(4,5);
+
+        if (addvalue == (4+5)) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+        int subtractvalue = calculator.subtract(4,5);
+
+        if (subtractvalue == (4-5)) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+    }
 }
