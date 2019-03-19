5cfbcdc23983fd22c1f66605302e82f87dd8dfd2
MarcinNidecki <marcin.nidecki90@gmail.com>
2019-03-19 19:40:26 +0100

    6.6

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsCalculation.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/statistics/StatisticsCalculation.java
@@ -2,8 +2,6 @@ package com.kodilla.testing.forum.statistics;
 
 public class StatisticsCalculation {
 
-    Statistics statistics;
-
 
     private int numberOfUsers;
     private int numberOfPost;
@@ -41,52 +39,30 @@ public class StatisticsCalculation {
         System.out.println("The average number of user comments: " + averageUserComments);
         System.out.println("Average number of comments per post: " + averagePostComments);
     }
+
     public int getNumberOfUsers() {
         return numberOfUsers;
-    }
-
-    public void setNumberOfUsers(int numberOfUsers) {
-        this.numberOfUsers = numberOfUsers;
     }
 
     public int getNumberOfPost() {
         return numberOfPost;
     }
 
-    public void setNumberOfPost(int numberOfPost) {
-        this.numberOfPost = numberOfPost;
-    }
-
     public int getNumberOfComments() {
         return numberOfComments;
-    }
-
-    public void setNumberOfComments(int numberOfComments) {
-        this.numberOfComments = numberOfComments;
     }
 
     public double getAverageUserPost() {
         return averageUserPost;
     }
 
-    public void setAverageUserPost(double averageUserPost) {
-        this.averageUserPost = averageUserPost;
-    }
-
     public double getAverageUserComments() {
         return averageUserComments;
-    }
-
-    public void setAverageUserComments(double averageUserComments) {
-        this.averageUserComments = averageUserComments;
     }
 
     public double getAveragePostComments() {
         return averagePostComments;
     }
 
-    public void setAveragePostComments(double averagePostComments) {
-        this.averagePostComments = averagePostComments;
-    }
 
 }
