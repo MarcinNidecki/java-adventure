b759bbb3dc0b463a00e97c943cc928783fddcd7f
MarcinNidecki <marcin.nidecki90@gmail.com>
2019-03-23 15:40:49 +0100

    7.6

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -6,15 +6,11 @@ import java.util.stream.IntStream;
 public interface ArrayOperations {
 
     static double getAverage(int[] numbers) {
-        // Average of int[]
-
-        double stream =  IntStream.range(0, numbers.length)
-                .reduce(0, (sum, current) -> sum = sum + numbers[current]);
         // print int[]
         IntStream.range(0, numbers.length)
                 .forEach(index -> System.out.print( numbers[index]+ " "));
-
-        return stream/numbers.length;
+        // return Average of int[]
+        return IntStream.of(numbers).average().getAsDouble();
 
 
 
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/portfolio/User.java
@@ -0,0 +1,43 @@
+package com.kodilla.stream.portfolio;
+
+public final class User {
+    private final String userName;
+    private final String realName;
+
+    public User(final String userName, final String realName) {
+        this.userName = userName;
+        this.realName = realName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", realName='" + realName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (userName != null ? !userName.equals(user.userName) : user.userName != null) return false;
+        return realName != null ? realName.equals(user.realName) : user.realName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
