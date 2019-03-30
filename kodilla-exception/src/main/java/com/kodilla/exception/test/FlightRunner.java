efdabd1d58935cb43974a06da5eca3f059fbdcf2
MarcinNidecki <marcin.nidecki90@gmail.com>
2019-03-30 10:35:23 +0100

    8.4

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightRunner.java
@@ -0,0 +1,17 @@
+package com.kodilla.exception.test;
+
+public class FlightRunner {
+
+    public static void main (String args[]) {
+
+        Flight flight = new Flight("Krakow","Warszawa");
+        FlightSearchEngine flightSearchEngine = new FlightSearchEngine();
+
+        try {
+            flightSearchEngine.findFilght(flight);
+        } catch (RouteNotFoundException e) {
+            System.out.println("no flights");
+        }
+
+    }
+}
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSearchEngine.java
@@ -0,0 +1,22 @@
+package com.kodilla.exception.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FlightSearchEngine {
+
+    public void findFilght(Flight flight) throws RouteNotFoundException {
+        Map<String, Boolean> airPorts = new HashMap<>();
+        airPorts.put("Krakow",true);
+        airPorts.put("Katowice", true);
+        airPorts.put("Warszawa",true);
+        airPorts.put("Radom", true);
+
+        if (airPorts.containsKey(flight.getArrivalAirport()) && airPorts.containsKey(flight.getDepartureAirport())) {
+            System.out.println("The fligh form " + flight.getArrivalAirport() + " to " + flight.getDepartureAirport() +
+                            " was found.");
+        } else {
+            throw new RouteNotFoundException("Flight was not found");
+        }
+    }
+}
