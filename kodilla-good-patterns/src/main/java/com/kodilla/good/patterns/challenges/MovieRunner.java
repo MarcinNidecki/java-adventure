70082260f50fb0308bce8c778a1f48d4f25a7c1b
MarcinNidecki <marcin.nidecki90@gmail.com>
2019-05-07 20:16:47 +0200

    13.1

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieRunner.java
@@ -0,0 +1,19 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class MovieRunner {
+
+    public static void main (String[] args) {
+        
+        MovieStore movieStore = new MovieStore();
+
+        String movieList = movieStore.getMovies().values().stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.joining("!","",""));
+
+        System.out.print(movieList);
+
+    }
+}
