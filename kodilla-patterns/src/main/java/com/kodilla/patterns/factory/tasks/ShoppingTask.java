86d7ea35f22a9e3261bad2c0dad21f17cbbec6a7
MarcinNidecki <marcin.nidecki90@gmail.com>
2019-05-20 18:48:22 +0200

    15.4 fix

--- kodilla-patterns/src/main/java/com/kodilla/patterns/factory/tasks/ShoppingTask.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/factory/tasks/ShoppingTask.java
@@ -1,12 +1,12 @@
 package com.kodilla.patterns.factory.tasks;
 
 public class ShoppingTask implements Task {
-    String taskName;
-    String whatToBuy;
-    double quantity;
-    boolean isExecuted = false;
+    private String taskName;
+    private String whatToBuy;
+    private double quantity;
+    private  boolean isExecuted = false;
 
-    public ShoppingTask(String taskName, String whatToBuy, double quantity) {
+    ShoppingTask(String taskName, String whatToBuy, double quantity) {
         this.taskName = taskName;
         this.whatToBuy = whatToBuy;
         this.quantity = quantity;
