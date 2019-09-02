ae825efe6dacf63f963e9cbc6061588c2c80eb52
MarcinNidecki <marcin.nidecki90@gmail.com>
2019-09-02 16:07:06 +0200

    30.3

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,13 +1,18 @@
 package com.kodilla.stream;
 
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
 
-
+        Map<String,Integer> m = new HashMap<>();
+        m.put("Test",1);
+        m.put("Test",1);
+        System.out.print(m.get("Test"));
 
     }
     public List<Integer> exterminate(List<Integer> numbers) {
